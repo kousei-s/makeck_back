29834ac5bf600e04512630c0229882925f81b32a
minato0129 <gororyo0129@gmail.com>
2024-11-27 12:16:11 +0900

    サンプルデータ入れたかった人生…

--- recipe/src/models/debug.go
+++ recipe/src/models/debug.go
@@ -4,6 +4,9 @@ import (
 	"recipe/utils"
 )
 
+var parallelt = true
+var parallelf = false
+
 var name1 = "キャベツ"
 var name2 = "豚肉"
 var name3 = "フライパン"
@@ -11,8 +14,8 @@ var name4 = "菜箸"
 var name5 = "中火で炒める"
 var name6 = "野菜炒め"
 var name7 = "下準備"
+var name8 = "全部煮る(nil)"
 var image1 = "D:\\Users\\Pictures\\IMG_mattuu.pwg"
-
 
 var name11 = "レタス"
 var name12 = "牛肉"
@@ -21,14 +24,35 @@ var name14 = "菜箸"
 var name15 = "強火で炒める"
 var name16 = "野菜ファイヤー"
 var name17 = "調理"
-
+var name18 = "冷蔵庫で冷やす"
 var image11 = "D:\\Users\\Pictures\\IMG_mattuu.pwg"
-	
+
+var name21 = "ブロッコリー"
+var name22 = "鶏肉"
+var name23 = "鍋"
+var name24 = "菜箸"
+var name25 = "中火で蒸す"
+var name26 = "ブロッコリー炒め"
+var name27 = "下準備"
+var name28 = "全部焼く"
+var image21 = "D:\\Users\\Pictures\\IMG_broccoli.pwg"
+
+var name31 = "ニンジン"
+var name32 = "魚"
+var name33 = "フライパン"
+var name34 = "菜箸"
+var name35 = "強火で焼く"
+var name36 = "ニンジンのソテー"
+var name37 = "調理"
+var name38 = "冷凍庫で冷やす"
+var image31 = "D:\\Users\\Pictures\\IMG_carrot.pwg"
+
 func RunDebug() {
 	// ここにデバック用のコードを書く
-	registration(name1,name2,name3,name4,name5,name6,name7,image1)
-	registration(name11,name12,name13,name14,name15,name16,name17,image11)
-
+	registration(name1, name2, name3, name4, name5, name6, name7, image1, parallelf, parallelt)
+	registration(name11, name12, name13, name14, name15, name16, name17, image11, parallelf, parallelt)
+	registration(name21, name22, name23, name24, name25, name26, name27, image21, parallelt, parallelf)
+	registration(name31, name32, name33, name34, name35, name36, name37, image31, parallelf, parallelt)
 
 	search()
 }
@@ -60,7 +84,7 @@ func search() {
 }
 
 // 登録の一連の流れ
-func registration(name1 string,name2 string, name3 string , name4 string , name5 string, name6 string,name7 string,image string) {
+func registration(name1 string, name2 string, name3 string, name4 string, name5 string, name6 string, name7 string, image string, parallel1 bool, parallel2 bool) {
 
 	utils.Println("材料登録１")
 	material1, err := Material_Register(MaterialArgs{
@@ -108,18 +132,31 @@ func registration(name1 string,name2 string, name3 string , name4 string , name5
 
 	utils.Println("手順登録１")
 	process1, err := Process_Register(ProcessArgs{
-		name:      name7,
+		name:        name7,
 		description: name5,
-		parallel:  false,
-		time:      10,
-		tools:     []Tools{tool1, tool2},
-		materials: []Material{material1, material2},
+		parallel:    parallel1,
+		time:        10,
+		tools:       []Tools{tool1, tool2},
+		materials:   []Material{material1, material2},
 	})
 	if err != nil {
 		utils.Println(err)
 	}
 	utils.Println(process1)
 
+	utils.Println("手順登録２")
+	process2, err := Process_Register(ProcessArgs{
+		name:        name7,
+		description: name5,
+		parallel:    parallel2,
+		time:        15,
+		tools:       []Tools{tool1, tool2},
+		materials:   []Material{material1, material2},
+	})
+	if err != nil {
+		utils.Println(err)
+	}
+	utils.Println(process2)
 
 	utils.Println("レシピ登録")
 	recipe, err := Recipe_Register(RecipeArgs{
@@ -130,7 +167,7 @@ func registration(name1 string,name2 string, name3 string , name4 string , name5
 			{Id: 1},
 			{Id: 2},
 		},
-		Prosecc: []Process{process1},
+		Prosecc:    []Process{process1},
 		LastSatate: Hot,
 	})
 
