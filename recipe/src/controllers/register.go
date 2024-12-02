e496067db1fa350563f9867ec27a47c91bb3b80c
mattuu0 <tempacountyt@gmail.com>
2024-12-02 09:23:56 +0900

    データの送信を追加

--- recipe/src/controllers/register.go
+++ recipe/src/controllers/register.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
+	"recipe/utils"
 
 	"github.com/labstack/echo/v4"
 )
@@ -12,7 +14,16 @@ type RegisterArgs struct {
 
 func RegisterRecipe(ctx echo.Context) error {
 	// jsonに変換
-	
+	val := new(RegisterArgs)
+	if err := ctx.Bind(val); err != nil {
+		//エラー処理
+		utils.Println("failed to bind json : " + err.Error())
+		return ctx.JSON(http.StatusBadRequest,echo.Map{
+			"result" : "failed to bind json",
+		})
+	}
+
+	log.Println(val.Name)
 
 	return ctx.JSON(http.StatusOK,echo.Map{
 		"result" : "success",
